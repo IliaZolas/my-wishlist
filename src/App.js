c1d420d42b8cba309a0c5a41c85e0642386c2340
Ilia Zolas <ilia@thedmcsa.co.za>
2021-12-28 15:03:29 +0100

    styles complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import editImage from './img/edit.svg'
+import deleteImage from './img/delete.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wishlist-title">
+        <h1>My Wishlist</h1>
+      </div>
+      <div className="wish-area">
+        <div>
+          <input className="text-box" type="text" placeholder="Type something..."/>
+        </div>
+        <div className="wish-list">
+          <div className="wish-card">
+            <div className="card-text">
+              <h2>My wish item here</h2>
+            </div>
+            <div className="wish-icons">
+              <div>
+                  <img src={editImage} className="card-icons" alt="edit" />
+              </div>
+              <div>
+                  <img src={deleteImage} className="card-icons" alt="edit" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
